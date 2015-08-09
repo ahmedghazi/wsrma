919566d3b39a0dff04dcfa1fc34e4b678da32a4f
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2015-08-09 17:30:29 +0200

    form url

--- config/schemas/ass.js
+++ config/schemas/ass.js
@@ -1,11 +1,24 @@
 var mongoose = require('mongoose');
 
 var Ass = mongoose.Schema({
-	index: true,
-    date_created: {type: Date, default: Date.now},
-    img: {unique: true, type: String},
-    ratings: {type: Array},
-    average: {type: Number}
+	//index: true,
+    date_created: {
+    	index: true,
+    	type: Date, default: Date.now
+    },
+    img: {
+    	unique: true, 
+    	index: true,
+    	type: String
+    },
+    ratings: {
+    	index: true,
+    	type: Array
+    },
+    average: {
+    	index: true,
+    	type: Number
+    }
 }, {
     //versionKey: false
 });
